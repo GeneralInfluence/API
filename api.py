00db234cf03a56d2eb5116922f2a962899c4feba
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2015-08-19 10:03:05 -0400

    first push to aws

--- api.py
+++ api.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """API to Data Community DC Data Lake built on Flask"""
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, make_response, current_app
 from werkzeug.exceptions import NotFound, Unauthorized, UnsupportedMediaType, BadRequest
 import boto
-import json
+import os, inspect
+import ujson as json
 import uuid
 import redis
 import amqp
-import ThreeScalePY
 import urllib, urllib2
 import sys
 import traceback
@@ -18,25 +18,56 @@ import magic
 import xlrd
 from logging import FileHandler
 from logging.handlers import RotatingFileHandler
+import ast
+from datetime import timedelta
+from functools import update_wrapper
+from flask import Flask
+from flask.ext.cors import CORS
+import time
+
+
+# from twisted.internet import task
+# from twisted.internet import reactor
+
 
 # Install Dependencies:
 # sudo apt-get update
 # sudo apt-get install python-setuptools python-libxml2
 # sudo easy_install pip
 # pip install flask boto redis celery filemagic xlrd
+# pip install -U flask-cors
 # Get the latest ThreeScale python library here: https://github.com/3scale/3scale_ws_api_for_python
 # and follow the directions to install
 
 from config import *
 
+# Assumes custom packages are either in the same directory or the parent directory for importing
+this_dir = os.path.realpath( os.path.abspath( os.path.split( inspect.getfile( inspect.currentframe() ))[0]))
+parent_dir = os.path.realpath(os.path.abspath(os.path.join(this_dir,"../")))
+sys.path.insert(0, this_dir)
+sys.path.insert(0, parent_dir)
+
+from IdeaNets.models.lstm.scode.lstm_class import LSTM as lstm
+from Preprocessing.loadCleanly import sheets as sh
+
+ROOT = "/v1/gizmoduck"
 app = Flask(__name__)
 app.config.from_object(__name__)
+CORS(app)
+# CORS(app, resources={ROOT + r"/*": {"origins": "*"}})
 
 if USESSL:
     from OpenSSL import SSL
     ctx = SSL.Context(SSL.SSLv23_METHOD)
     ctx.use_privatekey_file('ssl/ssl.key')
     ctx.use_certificate_file('ssl/ssl.cert')
+
+IDEANETS = {}
+# app.logger.info("Loading the model now.")
+# model = "untrustworthy"
+# model_path = os.path.realpath(os.path.abspath(os.path.join(this_dir,MODEL_MAPS[model])))
+# IDEANETS[model] = lstm() # I can see the type of model to use being an input at some point.
+# IDEANETS[model].load_pickle(model_path)
 
 MAX_MEGABYTES = 2
 app.config['MAX_CONTENT_LENGTH'] = MAX_MEGABYTES * 1024 * 1024
@@ -54,11 +85,54 @@ ERROR_MESSAGES = {404: "The requested resource cannot be found. Please check the
                   400: "The request is missing required parameters or otherwise malformed. Please check the API documentation and try again."
                   }
 
-ROOT = "/v1/documents"
 
 # Connections
 RED = redis.Redis(host=REDISHOST, port=REDISPORT, db=0)
-S3 = boto.connect_s3(aws_access_key_id=AWSKEY, aws_secret_access_key=AWSSECRET).get_bucket(BUCKET)
+# S3 = boto.connect_s3(aws_access_key_id=AWSKEY, aws_secret_access_key=AWSSECRET).get_bucket(BUCKET)
+
+# datagramRecieved = False
+# timeout = 1.0 # One second
+# def testTimeout():
+#     global datagramRecieved
+#     if not datagramRecieved:
+#         reactor.stop()
+#     datagramRecieved = False
+
+# import socket, errno, time
+# def maintain_socket():
+#     # setup socket to listen for incoming connections
+#     s = socket.socket()
+#     # 127.0.0.1
+#     # http://0.0.0.0:9999
+#     # s.bind(('localhost:9999', 1234))
+#     s.bind(('localhost', 1234))
+#     s.listen(1)
+#     remote, address = s.accept()
+#
+#     print "Got connection from: ", address
+#
+#     while 1:
+#         try:
+#             remote.send("message to peer\n")
+#             time.sleep(1)
+#         except socket.error, e:
+#             if isinstance(e.args, tuple):
+#                 print "errno is %d" % e[0]
+#                 if e[0] == errno.EPIPE:
+#                    # remote peer disconnected
+#                    print "Detected remote disconnect"
+#                 else:
+#                    # determine and handle different error
+#                    pass
+#             else:
+#                 print "socket error ", e
+#             remote.close()
+#             break
+#         except IOError, e:
+#             # Hmmm, Can IOError actually be raised by the socket module?
+#             print "Got IOError: ", e
+#             break
+
 
 def make_key(app_id, app_key, task_id):
     """This is a naming convention for both redis and s3"""
@@ -68,16 +142,12 @@ def check_credentials():
     """Check the credentials with 3scale. Return app_id and app_key pair if valid. 
        Raise Unauthorized error if not."""
     # request is a global variable from flask and contains all info about the request from the client.
-    if request.method == "GET":
-        app_id = request.args.get("app_id")
-        app_key = request.args.get("app_key")
-    elif request.method == "POST":
-        app_id = request.form.get("app_id")
-        app_key = request.form.get("app_key")
+
+    app_id  = str(request.args.get("app_id"))
+    app_key = str(request.args.get("app_key"))
     if app_id == None or app_key == None:
         raise Unauthorized()
-    authrep = ThreeScalePY.ThreeScaleAuthRep(THREESCALEKEY, app_id, app_key)
-    if authrep.authrep():
+    if (app_id in APP_IDS) & (app_key in APP_KEYS[app_id]):
         return app_id, app_key
     raise Unauthorized()
 
@@ -117,14 +187,55 @@ def validate_input_file(handle, mimetype):
             raise UnsupportedMediaType()
     handle.seek(0)
 
-def make_response(data, code, headers=None):
+def make_custom_response(data, code, headers=None):
     """Create a complete JSON response from an object using the Flask Response type"""
-    response = Response(json.dumps(data, indent=4)+"\n", status=code, mimetype='application/json')
-    response.headers["Server"] = "DC2 Data Lake API"
+    response = Response(json.dumps(data)+"\n", status=code, mimetype='application/json')
+    response.headers["Server"] = "General Influence API"
     if headers != None:
         for key, value in headers.items():
             response.headers[key] = value
     return response
+
+# def crossdomain(origin=None, methods=None, headers=None,
+#                 max_age=21600, attach_to_all=True,
+#                 automatic_options=True):
+#     if methods is not None:
+#         methods = ', '.join(sorted(x.upper() for x in methods))
+#     if headers is not None and not isinstance(headers, basestring):
+#         headers = ', '.join(x.upper() for x in headers)
+#     if not isinstance(origin, basestring):
+#         origin = ', '.join(origin)
+#     if isinstance(max_age, timedelta):
+#         max_age = max_age.total_seconds()
+#
+#     def get_methods():
+#         if methods is not None:
+#             return methods
+#
+#         options_resp = current_app.make_default_options_response()
+#         return options_resp.headers['allow']
+#
+#     def decorator(f):
+#         def wrapped_function(*args, **kwargs):
+#             if automatic_options and request.method == 'OPTIONS':
+#                 resp = current_app.make_default_options_response()
+#             else:
+#                 resp = make_response(f(*args, **kwargs))
+#             if not attach_to_all and request.method != 'OPTIONS':
+#                 return resp
+#
+#             h = resp.headers
+#
+#             h['Access-Control-Allow-Origin'] = origin
+#             h['Access-Control-Allow-Methods'] = get_methods()
+#             h['Access-Control-Max-Age'] = str(max_age)
+#             if headers is not None:
+#                 h['Access-Control-Allow-Headers'] = headers
+#             return resp
+#
+#         f.provide_automatic_options = False
+#         return update_wrapper(wrapped_function, f)
+#     return decorator
 
 @app.errorhandler(401)
 @app.errorhandler(404)
@@ -136,7 +247,8 @@ def handle_user_error(err):
     message = {"status" : "fail",
         "data" : {"reason" : ERROR_MESSAGES[err.code]}}
     app.logger.warning("A user error occurred. code: %i, path: %s", err.code, request.path)
-    return make_response(message, err.code)
+    # return make_custom_response(message, err.code)
+    return make_response(str(message))
 
 @app.errorhandler(500)
 def handle_internal_error(err):
@@ -148,17 +260,29 @@ def handle_internal_error(err):
                                      exc_value, 
                                      exc_traceback)
     exc = ''.join(exc)
-    error_message = {"api_user": "",
-           "api_key": "",
+    error_message = {
+           "api_user":"",
+           "api_key":"",
            "to": SUPPORTEMAIL,
-           "from": DC2EMAIL,
-           "subject": "DC2 Data Lake API Error",
-           "text": "An error ocurred in the DC2 Data Lake API: \n\nENDPOINT: " \
-                + request.path + " " + request.method + "\n\n" + exc}
+           "from": SUPPORTEMAIL,
+           "subject": "General Influence API Error",
+           "text": "An error ocurred in the General Influence IdeaNet API: \n\nENDPOINT: " \
+                + request.path + " " + request.method + "\n\n" + exc +
+                "\n\n" + "app_id:" + str(request.args.get("app_id")) +
+                "\n\n" + "app_key:" + str(request.args.get("app_key")) }
     data = urllib.urlencode(error_message)
-    urllib2.urlopen(url="https://api.sendgrid.com/api/mail.send.json", data=data).read()  
+    # urllib2.urlopen(url="https://api.sendgrid.com/api/mail.send.json", data=data).read()  # NEED A NEW EMAIL SOLUTION
+    # smtp_handler = logging.handlers.SMTPHandler(mailhost=("smtp.gmail.com", 465),
+    #                                             fromaddr="sean@general-influence.com",
+    #                                             toaddrs="sean.moore.gonzalez@gmail.com",
+    #                                             subject="General Influence API Error")
+    #
+    #
+    # logger = logging.getLogger()
+    # logger.addHandler(smtp_handler)
     app.logger.error("ERROR: " + error_message["text"])      
-    return make_response(message, 500)
+    # return make_custom_response(message, 500)
+    return make_response(str(message))
 
 def post_initial_status(key):
     """Post the first status message to Redis. Redis messages are used by the queue endpoint."""
@@ -204,11 +328,161 @@ def submit_job(app_id, app_key, task_id, mimetype, text_col, dedupe):
     cha.close()
     conn.close()
 
+def get_models(models):
+    """
+    The models would be stored in S3, or perhaps for now just on the server in a file.
+    Eventually they might be in a database, but either way I need to reduce their size because they take a while to load.
+    That being said, I really should have a login capability where we use the user information to gather and load the necessary models.
+    I could have those models loaded already using celery
+    :return:
+    """
+    models_loaded = []
+    for model in models:
+        if (model in MODEL_MAPS):
+            models_loaded.append(model)
+            if (model not in IDEANETS):
+                model_path = os.path.realpath(os.path.abspath(os.path.join(this_dir,MODEL_MAPS[model])))
+                IDEANETS[model] = lstm() # I can see the type of model to use being an input at some point.
+                IDEANETS[model].load_pickle(model_path)
+                app.logger.info("Model(s) loaded: " + str(model))
+
+    return models_loaded
+
 # The @app.route function decorators map endpoints to functions.
-@app.route(ROOT, methods=['POST', 'GET'])
-def documents():
-    """The POST method for this endpoint is where API clients submit jobs.
-    The GET method returns a list of previous task ids."""
+# @crossdomain(origin='*')
+# @cross_origin()
+@app.route(ROOT + '/initialize/', methods=['POST', 'GET'])
+def initialize_session():
+    """
+    Need to get things loaded so the user response is quick!
+
+    The POST method gives the user information for model retrieval
+    The GET method gives the status of loading the models
+    :return:
+    """
+
+    # l = task.LoopingCall(testTimeout)
+    # l.start(timeout) # call every second
+    #
+    # reactor.run()
+
+    if request.method == 'POST':
+        # maintain_socket()
+        app.logger.info("Got a file POST request") # Log that we got a request
+        app_id, app_key = check_credentials() # Extract and validate credentials.
+        params = ast.literal_eval(request.data)
+        models = params['models']
+        models_loaded = get_models(models)
+        data = {"status":"success",
+                "models_loaded":models_loaded}
+
+    if request.method == 'GET':
+        print "This is a placeholder for getting model loading status"
+        data = {"status":"success",
+                "models_loaded":[]}
+
+    # l.stop() # will stop the looping calls
+    app.logger.info(request.data)
+    # return make_custom_response(data,200)
+    return make_response(str(data))
+
+# @crossdomain(origin='*')
+# @cross_origin()
+@app.route(ROOT + '/classify_one/', methods=['POST', 'GET'])
+def classify_json():
+    """
+    The POST method receives a json and calls the classifier
+    The GET method simply returns the mdoels available to this user.
+
+    :param request.data: dictionary with fields 'sentences' and 'model',
+        which indicates which ideanet model to use on the given sentences.
+    :return:
+    """
+    app.logger.info("Testing testing 123")
+    if request.method == 'GET':
+        ### FORMALITIES FORMALITIES FORMALITIES FORMALITIES
+        app.logger.info("Got a file POST request") # Log that we got a request
+        app.logger.info(request.data)
+        app_id, app_key = check_credentials() # Extract and validate credentials.
+
+        data = ast.literal_eval(request.data)
+        model_initialized = True
+        try:
+            sentences = data['sentences']
+            models = data['model']
+            predictions = []
+            for model in models:
+                if (model in MODEL_MAPS):
+                    print model
+                    print IDEANETS
+                    if model not in IDEANETS:
+                        model_initialized = False
+                        models_loaded = get_models(models)
+                        if model in models_loaded:
+                            model_initialized = True
+                    if model_initialized:
+                        ideanet = IDEANETS[model]
+                        predictions += ideanet.classify(sentences).tolist()
+            data = {"status":"success",
+                    "predictions":predictions}
+
+        except:
+            data = {"status":"failure",
+                    "predictions":[]}
+
+    # if request.method == 'GET':
+        # This is a placeholder for a more informative endpoint.
+    print "data: " + str(data)
+    # time.sleep(5)
+    # return make_custom_response(data, 200)
+    return make_response(str(data))
+
+# The @app.route function decorators map endpoints to functions.
+@app.route(ROOT + '/mass', methods=['POST', 'GET'])
+def classify_files():
+    """
+    The POST method allows API clients to use their model(s)
+    The GET method allows API clients to get a list of available models.
+
+    :return:
+    """
+    if request.method == "POST":
+        # Log that we got a request
+        app.logger.error("Got a file POST request")
+        # Extract and validate credentials.
+        app_id, app_key = check_credentials()
+
+        try:
+            submitted_file = request.files.get('file')
+            ctype = submitted_file.content_type
+            if ctype not in ACCEPTED_MIMETYPES:
+                app.logger.error("Unsupported Media Type: %s", ctype)
+                raise UnsupportedMediaType
+
+            data = json.loads(request.data)
+            if 'textcol' in data:
+                textcol = data['textcol']
+            validate_input_file(submitted_file, ctype)
+            header, rows = sh.get_spreadsheet_rows(submitted_file,textcol,dedupe=True)
+
+            model = data['model']
+            ideanet = IDEANETS[model]
+            predictions = ideanet.classify(rows)
+            data = {"status":"success",
+                    "data":predictions}
+
+        except:
+            data = {"status":"failure",
+                    "data":{}}
+
+
+    # elif request.method == "GET":
+
+@app.route(ROOT + '/train', methods=['POST', 'GET'])
+def train_model():
+    """
+    The POST method is where API clients provide data to train their model
+    The GET method provides training status (statii?) and details for calling their model when complete."""
     if request.method == "POST":
         # Log that we got a request
         app.logger.error("Got a file POST request")
@@ -255,7 +529,8 @@ def documents():
                                     "type": "application/json"}]}}
 
         app.logger.error("File successfully submitted. type: %s, size: %i, app_id: %s, task_id: %s, dedupe: %s", ctype, size, app_id, task_id, dedupe)
-        return make_response(data, 202, headers = {"Location": ROOT + "/queue/" + task_id})
+        # return make_custom_response(data, 202, headers = {"Location": ROOT + "/queue/" + task_id})
+        return make_response(str(data))
 
     if request.method == "GET":
         # Extract and validate credentials. 
@@ -282,7 +557,8 @@ def documents():
                                           "href": ROOT + "/" + task_id,
                                           "type" : "application/json"})
 
-        return make_response(data, 200)
+        # return make_custom_response(data, 200)
+        return make_response(str(data))
 
 
 @app.route(ROOT + '/queue/<string:task_id>', methods=['GET'])
@@ -328,7 +604,8 @@ def queue(task_id):
                                  "type": "application/json"}]
 
     app.logger.info("Polling task_id: %s, status: %s, percentComplete: %i", task_id, status["documentStatus"], status["percentComplete"])
-    return make_response(data, code, headers)
+    # return make_custom_response(data, code, headers)
+    return make_response(str(data))
 
 @app.route(ROOT + '/<string:task_id>', methods=['GET'])
 def list_results(task_id):
@@ -360,7 +637,8 @@ def list_results(task_id):
                                       # For some reason expiry_date doesn't work so it's gone for now
                                       #"expiry_date": key.expiry_date})
     
-    return make_response(data, 200)
+    # return make_custom_response(data, 200)
+    return make_response(str(data))
 
 def get_file(task_id, name):
     """Helper function for spitting out a file on s3"""
@@ -394,8 +672,8 @@ if __name__ == '__main__':
     app.logger.addHandler(handler)
     if USESSL:
         #app.run(host='0.0.0.0', port=FLASKPORT, ssl_context=ctx)
-        app.run(host='0.0.0.0', port=FLASKPORT, ssl_context='adhoc')
+        app.run(host='0.0.0.0', port=FLASKPORT, ssl_context='adhoc', threaded=True)
     else:
-        app.run(host='0.0.0.0', port=FLASKPORT)
+        app.run(host='0.0.0.0', port=FLASKPORT, debug=DEBUG, threaded=True)
 
 
