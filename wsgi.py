d8ed8cc770e35d21edbd73ec712c7ffe2c97c88c
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2015-08-19 13:50:51 -0400

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python 2
 """nginx server"""
 
-import os, sys, inspect
-
-this_dir = os.path.realpath( os.path.abspath( os.path.split( inspect.getfile( inspect.currentframe() ))[0]))
-parent_dir = os.path.realpath(os.path.abspath(os.path.join(this_dir,"../")))
-sys.path.insert(0, this_dir)
-sys.path.insert(0, parent_dir)
-
 from api import app
 
 if __name__ == "__main__":
