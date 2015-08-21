5e6e2bbe747715f7a22c5b258cea817b7d95e42e
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2015-08-21 10:32:46 -0400

    continuing nginx work

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -6,4 +6,5 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    # application.run(host='0.0.0.0')
+    application.run(host='127.0.0.1')
